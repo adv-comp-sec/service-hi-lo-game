9abc557c849ecc09e31727cadfb92c5c3e92e1b5
HYEWONLEE\hyewon <katelavande@gmail.com>
2022-11-21 23:02:31 -0500

    create a sender socket in the client

--- HiLoGame_Client/MainWindow.xaml.cs
+++ HiLoGame_Client/MainWindow.xaml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -27,7 +28,7 @@ namespace HiLoGame_Client
             Instructions.Text = "This is Hi-Lo game.";
         }
 
-        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        private void ConnectButton_Click(object s, RoutedEventArgs e)
         {
             // Validate the user name
             String name = Name.Text;
@@ -39,31 +40,44 @@ namespace HiLoGame_Client
 
             // Validate the IP Address
             String strIP = IPAdd.Text;
+            IPAddress IP;
             if (strIP == "")
             {
                 Instructions.Text = "[ERROR: You must enter the IP Address to start.]";
                 return;
             }
-            else if (!IPAddress.TryParse(strIP, out IPAddress ip))
+            else if (!IPAddress.TryParse(strIP, out IP))
             {
                 Instructions.Text = "[ERROR: You must enter the right form of the IP Address. (e.g. 127.0.0.1)]";
                 return;
             }
+            else
+            {
+                IP = IPAddress.Parse(strIP);
+            }
 
             // Valiate the port number
             String strPort = PortNumber.Text;
+            int port;
             if (strPort == "")
             {
                 Instructions.Text = "[ERROR: You must enter the port number to start.]";
                 return;
             }
-            else if (!int.TryParse(strPort, out int port))
+            else if (!int.TryParse(strPort, out port))
             {
                 Instructions.Text = "[ERROR: You must enter the right form of the port number.]";
                 return;
             }
+            else
+            {
+                port = Convert.ToInt32(strPort);
+            }
 
-
+            // Start the game.
+            Instructions.Text = "Hello, " + name + "! Let's start a game!\n";
+            Instructions.Text += "Try to connect into " + IP.ToString() + ":" + port.ToString() + " server...\n";
+            Socket sender = new Socket(IP.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
         }
     }
 }

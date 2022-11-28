53d1a450589e99a4f5d1516418f957e871764c36
Maísa Wolff Resplande <maisawres@gmail.com>
2022-11-28 17:55:49 -0500

    Create an event log.

---
+++ W11Service/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W11Service
+{
+    public static class Logger
+    {
+        public static void Log(string message)
+        {
+            EventLog serviceEventLog = new EventLog();
+
+            if(!EventLog.SourceExists("ServiceEventSource"))
+            {
+                EventLog.CreateEventSource("ServiceEventSource", "ServiceEventLog");
+            }
+
+            serviceEventLog.Source = "ServiceEventSource";
+            serviceEventLog.Log = "ServiceEventLog";
+            serviceEventLog.WriteEntry(message);
+        }
+    }
+}
